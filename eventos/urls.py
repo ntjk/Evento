9e074ed6b1daf43c5d0f4085d1d925269a45a690
ntjk <nattlyjk@gmail.com>
2019-07-14 01:30:25 -0700

    las vistas y funcionalidad de crear evento
    las vistas incluyen las de: eventos, asistentes, ponencias, fechas, ponentes, etc.

--- eventos/urls.py
+++ eventos/urls.py
@@ -2,8 +2,13 @@ from django.urls import path
 from . import views
 app_name = 'eventos'
 urlpatterns = [
-	path('', views.IndexView.as_view(), name='index'),
-	path('ponencias', views.ponencias, name='ponencias'),
+	#path('', views.IndexView.as_view(), name='index'),
+	path('', views.eventos, name='index'),
+	path('<int:evento_id>/ponencias', views.ponencias, name='ponencias'),
+	path('<int:evento_id>/asistentes', views.asistentes, name='asistentes'),
+	path('<int:evento_id>/fechas', views.fechas, name='fechas'),
+	path('<int:ponencia_id>/ponentes', views.ponentes, name='ponentes'),
+	path('crearEvento', views.crearEvento, name='crearEvento'),
 	#path('<int:pk>/', views.DetalleView.as_view(), name='detalle'),
 	#path('<int:pk>/ponencias/', views.PonenciasView.as_view(), name='ponencias'),
 	#path('', views.index, name='index'),
--- eventos/views.py
+++ eventos/views.py
@@ -7,16 +7,46 @@ from .models import Evento, Ponencia, Ponente, Fecha, Asistente
 from django.utils import timezone
 
 
-class IndexView(generic.ListView):
+'''class IndexView(generic.ListView):
 	template_name = 'eventos/index.html'
 	context_object_name = 'lista_eventos'
 	def get_queryset(self):
-		return Evento.objects.order_by('-id')#.filter(pub_date__lte=timezone.now())
-
+		return Evento.objects.order_by('-id'))'''
+	
 '''def ponentes(request):
     return render(request, 'eventos/ponentes.html', {'ponentes': Ponente.objects.all()})
     def get_queryset(self):
     	return Ponente.objects.filter()'''
 
-def ponencias(request):
-    return render(request, 'eventos/ponencias.html', {'ponencias': Ponencia.objects.all()})
+def eventos(request):
+	return render(request, 'eventos/index.html', {'eventos': Evento.objects.all()}) 
+
+def ponencias(request, evento_id):
+	if evento_id == 0:
+		return render(request, 'eventos/ponencias.html', {'ponencias': Ponencia.objects.all()})
+	else:
+		return render(request, 'eventos/ponencias.html', {'ponencias': Ponencia.objects.filter(evento_id=evento_id)})
+
+def ponentes(request, ponencia_id):
+	if ponencia_id == 0:
+		return render(request, 'eventos/ponentes.html', {'ponentes': Ponente.objects.all()})
+	else:
+		return render(request, 'eventos/ponentes.html', {'ponentes': Ponente.objects.filter(ponencia_id=ponencia_id)})
+
+def fechas(request, evento_id):
+	return render(request, 'eventos/fechas.html', {'fechas': Fecha.objects.filter(evento_id=evento_id)})
+
+def asistentes(request, evento_id):
+	return render(request, 'eventos/asistentes.html', {'asistentes': Asistente.objects.filter(evento_id=evento_id)})
+
+def crearEvento(request):
+	try:
+		nombre = request.POST['firstname']
+	except (KeyError, Evento.DoesNotExist):
+		return render(request, 'eventos/crearEvento.html', {
+			'error_message': "No ingreso nombre del evento",
+		})
+	else:
+		nuevo_evento = Evento(nombre_evento = nombre)
+		nuevo_evento.save()
+	return HttpResponseRedirect(reverse('eventos:index'))
