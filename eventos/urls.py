1aa4255c5e155310e0f1105fd81462f5f0568768
ntjk <nattlyjk@gmail.com>
2019-07-14 20:57:04 -0700

    login y logout

--- eventos/urls.py
+++ eventos/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 app_name = 'eventos'
+
+
 urlpatterns = [
 	#path('', views.IndexView.as_view(), name='index'),
 	path('', views.eventos, name='index'),
